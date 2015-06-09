bfb16708e1e956ba5463d8aab7851df92a232a87
Andrew Shaydurov <gearhead@inbox.ru>
2015-06-09 18:59:05 +0900

    Update url_helper.rb

--- app/helpers/mailkick/url_helper.rb
+++ app/helpers/mailkick/url_helper.rb
@@ -1,11 +1,14 @@
 module Mailkick
   module UrlHelper
     def mailkick_unsubscribe_url
+      user = Mailkick.user_method.call message.to.first
+      verifier = ActiveSupport::MessageVerifier.new(Mailkick.secret_token)
+      token = verifier.generate([message.to.first, user.try(:id), user.try(:class).try(:name), nil])
       Mailkick::Engine.routes.url_helpers.url_for(
         (ActionMailer::Base.default_url_options || {}).merge(
           controller: "mailkick/subscriptions",
           action: "unsubscribe",
-          id: "{{MAILKICK_TOKEN}}"
+          id: CGI.escape(token)
         )
       )
     end
